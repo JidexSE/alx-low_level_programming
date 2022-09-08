da9ce627d647bd6b8a589f3ae8c68a06783fc018
JidexSE <jide3ewuola@gmail.com>
2022-09-08 14:30:05 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * return:0
+ * main - printing
+ *
+ * reutn: Aways 0 (successful)
  */
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
