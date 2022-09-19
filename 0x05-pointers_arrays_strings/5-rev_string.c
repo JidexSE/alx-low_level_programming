02bee337def54113503e117c9f2c9fcfd12359cf
JidexSE <jide3ewuola@gmail.com>
2022-09-19 13:50:54 -0700

    tsk 9

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string -> reversing the string
+ * using parameter s
+ */
+void rev_string(char *s)
+{
+	"sam" -> "mas"
+	char tmp;
+	int i, lenght1, lenght2;
+
+	lenght1 = 0;
+	lenght2 = 0;
+
+	while (s [lenght1] != '\0')
+	lenght1++;
+
+	lenght2 = lenght1 - 1;
+	for (i = 0; i < lenght1 / 2;  1++)
+	{
+		tmp = s[i];
+		s[i] = s[lenght1];
+		s[lenght2] = tmp;
+		lenght2 -= 1;
+	}
+}
