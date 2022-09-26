6c74c8b6b36f41c3bfac618a328f892183f14c55
JidexSE <jide3ewuola@gmail.com>
2022-09-26 13:53:47 -0700

    ttt

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,3 +1,5 @@
+cat > _putchar.c
+
 #include "main.h"                                                                                                                    
 #include <unistd.h>                                                                                                                  
                                                                                                                                      
@@ -8,7 +10,8 @@
  * Return: On success 1.                                                                                                             
  * On error, -1 is returned, and errno is set appropriately.                                                                         
  */                                                                                                                                  
-int _putchar(char c)                                                                                                                 
+int _putchar(char c)                                                                                                    
 {                                                                                                                                    
-        return (write(1, &c, 1));                                                                                                    
+        return (write(1, &c, 1)); 
+
 }
