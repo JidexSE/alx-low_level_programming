986fb5281bde52ec593e165babfedc2a03b3ab28
JidexSE <jide3ewuola@gmail.com>
2022-09-26 18:26:41 -0700

    0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-/* using MAIN_H in place of main.h defining it as a 'macro'*/
-
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,6 +7,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-
-
-#endif
+void set_string(char **s, char *to);
