46a87dd1699d9c0df4ee4a2bf9e70c35f6730ba5
JidexSE <jide3ewuola@gmail.com>
2022-09-26 18:00:16 -0700

    1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,20 +2,8 @@
 #define MAIN_H
 /* using MAIN_H in place of main.h defining it as a 'macro'*/
 
-void print_array(int *a, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *s);
-char *cap_string(char *s);
-char *leet(char *s);
-char *rot13(char *s);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 #endif
