bfd9fdba8564626914641bfbac75852833f1fbf8
JidexSE <jide3ewuola@gmail.com>
2022-09-26 18:09:25 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - function to match strings from accept with part of s
+ * @s: larger string
+ * @accept: smaller string
+ * Return: an integer number that counts from 1 to where accept stops
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int counter;
+
+	for (i = 0; s[i] != '\0'; i++) /*check through the whole string*/
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j]) /* if the strings are equal at i and j*/
+			{
+				counter += 1; /* add 1 to counter */
+			}
+		}
+	}
+	return (counter);
+}
