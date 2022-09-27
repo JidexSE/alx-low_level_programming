e7eadd14f4f76e8cf16ad5aed0d97ce2a5a33119
JidexSE <jide3ewuola@gmail.com>
2022-09-26 17:54:48 -0700

    intial

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,24 +1,21 @@
-#ifndef MAIN_H                                                                                                                     
-#define MAIN_H                                                                                                                     
-                                                                                                                                   
-/**                                                                                                                                
- * File: main.h                                                                                                                    
- * Auth: sam tech (samuel birhanu)                                                                                                 
- * description: Header file containing prototypes for all functions                                                                
- *       used in the 0x06-pointers_arrays_strings directory.                                                                       
- */                                                                                                                                
-                                                                                                                                   
-#include <stddef.h>                                                                                                                
-                                                                                                                                   
-int _putchar(char c);                                                                                                              
-void *_memset(void *s, int c, size_t n);                                                                                           
-char *_memcpy(char *dest, char *src, unsigned int n);                                                                              
-char *_strchr(char *s, char c);                                                                                                    
-unsigned int _strspn(char *s, char *accept);                                                                                       
-char *_strpbrk(char *s, char *accept);                                                                                             
-char *_strstr(char *haystack, char *needle);                                                                                       
-void print_chessboard(char (*a)[8]);                                                                                               
-void print_diagsums(int *a, int size);                                                                                             
-void set_string(char **s, char *to);                                                                                               
-                                                                                                                                   
+#ifndef MAIN_H
+#define MAIN_H
+/* using MAIN_H in place of main.h defining it as a 'macro'*/
+
+void print_array(int *a, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
+char *rot13(char *s);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+
+
 #endif
